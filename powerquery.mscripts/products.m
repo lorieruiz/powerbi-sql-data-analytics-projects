fb146444af00d813bbfe30c0dcd7f10f107b372f
lorieruiz <lorieruiz90@gmail.com>
2025-07-18 14:25:06 +0800

    m.scripts_inputs

---
+++ powerquery.mscripts/products.m
@@ -0,0 +1,6 @@
+// =====================
+    // THIS SUMMARIZES DIFFERENT POWER QUERY SCRIPTS - (M)
+    // Note: points to specific files
+
+DOWNTIME ANALYSIS
+[downtime_factor-M-Script] (./powerquery.mscripts/downtime_analysis/downtime_factor.m/)
